13d63a6a5001ad1566b1009bf79e018203af8714
sqqihao@gmail.com <sqqihao@gmail.com>
2016-01-26 18:44:47 +0800

    use requireJS

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,6 @@
  * 主要的main方法， 整个编辑器的初始化和事件的控制， 以及导出等；
  * 左侧的视图， 右侧的可编辑内容；
  * */
-
-
-
 define(["PubData", "EpubBuilder"], function(PubData, EpubBuilder) {
     var pubData = new PubData("#left-nav","#content-nav");
     var epub = new EpubBuilder();
--- static/third/umeditor/_src/ui/separator.js
+++
@@ -1,9 +0,0 @@
-//button 类
-UM.ui.define('separator', {
-    tpl: '<div class="edui-separator" unselectable="on" onmousedown="return false" ></div>',
-    init: function (options) {
-        var me = this;
-        me.root($($.parseTmpl(me.tpl, options)));
-        return me;
-    }
-});
